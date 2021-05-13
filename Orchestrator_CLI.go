c2b096b7dcd1e625e6ad69c6d36ce9cb98007a28
Amatus Muscheites <amatus.muscheites@gmail.com>
2021-05-13 09:38:44 +0200

    added color dependency, added very basic status report functionality

--- Orchestrator_CLI.go
+++ Orchestrator_CLI.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"github.com/fatih/color"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 )
 
+type TestStatus string
+
+const (
+	SUCCESS   TestStatus = "SUCCESS"
+	PENDING              = "PENDING"
+	FAILED               = "FAILED"
+	EXECUTING            = "EXECUTING"
+)
+
 var (
-	START = "start"
-	STOP = "stop"
+	START  = "start"
+	STOP   = "stop"
 	UPDATE = "update"
 	STATUS = "status"
 )
 
+type TestStatusReport struct {
+	Name      string                      `json:"name"`
+	Status    TestStatus                  `json:"status"`
+	Message   string                      `json:"message,omitempty"`
+	StartTime string                      `json:"startTime"`
+	Children  map[string]TestStatusReport `json:"children,omitempty"`
+}
+
+type PrintFuncType func(string, ...interface{})
+
+var outputColors = map[TestStatus]PrintFuncType{
+	SUCCESS:   color.Green,
+	FAILED:    color.Red,
+	PENDING:   color.Yellow,
+	EXECUTING: color.Blue,
+}
+
 func main() {
+
 	var hostname, port, action, testName string
 	flag.StringVar(&hostname, "hostname", "localhost", "hostname")
 	flag.StringVar(&port, "port", "8080", "port number")
@@ -24,6 +53,7 @@ func main() {
 	flag.StringVar(&testName, "test_name", "", "name of test to perform action on")
 	flag.Parse()
 
+	outputColors[SUCCESS]("hi")
 	if action == "" {
 		printErrorForFlag("action")
 	}
@@ -36,11 +66,36 @@ func main() {
 	case START:
 		startTest(hostname, port, testName)
 		break
+	case STATUS:
+		getAndPrintTestStatus(hostname, port, testName)
 	default:
 		flag.PrintDefaults()
 		fmt.Printf("\nInvalid argument for 'action', possible values: %s, %s, %s, %s\n", START, STOP, UPDATE, STATUS)
 		os.Exit(-1)
 	}
+}
+
+func getAndPrintTestStatus(hostname string, port string, name string) {
+	connString := fmt.Sprintf("http://%s:%s/test/status", hostname, port)
+	response, err := http.Get(connString)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	printStatusResponse(response)
+}
+
+func printStatusResponse(response *http.Response) {
+	message, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(message))
+	msgBody := TestStatusReport{}
+	err = json.Unmarshal(message, &msgBody)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(msgBody)
 }
 
 func startTest(hostname, port, testName string) {
@@ -60,7 +115,7 @@ func printResponse(response *http.Response) {
 	fmt.Println(string(message))
 }
 
-func printErrorForFlag(flagName string){
+func printErrorForFlag(flagName string) {
 	flag.PrintDefaults()
 	fmt.Println(fmt.Errorf("\nMissing value for argument %s\n", flagName))
 	os.Exit(-1)
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "Orchestrator_CLI"
+module Orchestrator_CLI
+
+go 1.16
+
+require github.com/fatih/color v1.10.0 // indirect
